d52025bee0e2fb93659a4f739ab27290cc11c9c7
vijaybaliah <vijaybalu1989@gmail.com>
2020-01-11 14:33:58 +0530

    refactor: error naming

--- backend/src/app.py
+++ backend/src/app.py
@@ -67,7 +67,7 @@ def create_app():
             abort(STATUS_UNPROCESSABLE)
 
     @app.errorhandler(INTERNAL_SERVER_ERROR)
-    def internalserver(error):
+    def internal_server(error):
         return jsonify({
             "success": False,
             "error": INTERNAL_SERVER_ERROR,
