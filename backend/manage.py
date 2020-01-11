ffe6c7cc3bec97f9520a7a912991230048d63a4a
vijaybaliah <vijaybalu1989@gmail.com>
2020-01-11 10:51:43 +0530

    build: manage

--- backend/manage.py
+++ backend/manage.py
@@ -1,8 +1,22 @@
-from flask import Flask
-from src.database.models import setup_db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-app = Flask(__name__)
-setup_db(app)
+from src.app import app
+from src.database.models import db
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+# from flask import Flask
+# from src.database.models import setup_db
+
+# app = Flask(__name__)
+# setup_db(app)
+
+# if __name__ == '__main__':
+#  app.run(debug=True)
